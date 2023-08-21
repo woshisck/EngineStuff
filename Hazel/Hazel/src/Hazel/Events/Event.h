7fc8e97ea7f697e44f2696a13c77bd58c6834d59
woshisck <373768752@qq.com>
2023-08-21 19:32:23 +0800

    udpate event

--- Hazel/Hazel/src/Hazel/Events/Event.h
+++ Hazel/Hazel/src/Hazel/Events/Event.h
@@ -26,8 +26,11 @@ namespace Hazel {
 		EventCategoryMouseButton = BIT(4)
 	};
 
-#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() {return EventType::##type; } \
-								virtual EventType
+#define EVENT_CLASS_TYPE(type)  static EventType GetStaticType() {return EventType::##type; } \
+								virtual EventType GetEventType() const override {return GetStaticType();} \
+								virtual const char* GetName() const override {return #type;}
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override {return category;}
 
 	class HAZEL_API Event {
 		friend class EventDispatcher;
@@ -45,6 +48,29 @@ namespace Hazel {
 
 	};
 
+	class EventDispatcher {
+		template<typename T>
+		using EventFn = std::function<bool(T&)>;
+	public:
+		EventDispatcher(Event& event) : m_Event(event){
 
+		}
 
+		template<typename T>
+		bool Dispatch(EventFn<T> func) {
+			if (m_Event.GetEventType() == T::GetStaticType()) {
+				m_Event.m_handled = func(*(T*)&m_Event);
+				return true;
+			}
+			return false;
+		}
+	private:
+		Event& m_Event;
+
+	};
+
+	inline std::ostream& operator << (std::ostream& os, const Event& e) {
+		return os << e.ToString();
+	}
 }
+
--- Hazel/Hazel/src/Hazel/Events/KeyEvent.h
+++ Hazel/Hazel/src/Hazel/Events/KeyEvent.h
@@ -1,2 +1,11 @@
 #pragma once
 
+#include "Event.h"
+
+#include <sstream>
+
+namespace Hazel {
+	class HAZEL_API KeyEvent : public Event {
+		//inline int GetKeyCode() const { return m_KeyCode; }
+	};
+}
